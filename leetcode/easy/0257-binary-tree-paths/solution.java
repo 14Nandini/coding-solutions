2b9ab3ac56137fe806a1e52ef50d168b12e2aa58
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-04 20:01:14 +0530

    ✅ Binary Tree Paths | Easy | Java | Runtime: 5 ms (35.40%) | Memory: 49.7 MB | 2026-09-04

---
+++ leetcode/easy/0257-binary-tree-paths/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root != null) findPaths(root, "", res);
+        return res;
+    }
+
+    private void findPaths(TreeNode node, String path, List<String> res) {
+        path += (path.isEmpty() ? "" : "->") + node.val;
+        
+        if (node.left == null && node.right == null) {
+            res.add(path);
+            return;
+        }
+        
+        if (node.left != null)  findPaths(node.left, path, res);
+        if (node.right != null) findPaths(node.right, path, res);
+    }
+}
