d1887a90474a797e4ffde26d141141a034b98129
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-13 18:34:01 +0530

    ✅ Remove Duplicates from an array | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

--- gfg/easy/remove-duplicates-in-small-prime-array/solution.java
+++ gfg/easy/remove-duplicates-in-small-prime-array/solution.java
@@ -3,8 +3,6 @@ class Solution {
         // code here
         HashSet<Integer> hs = new HashSet<>();
         for(int num : arr) hs.add(num);
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int num : hs)  res.add(num);
-        return res;
+        return new ArrayList(hs);
     }
 }
