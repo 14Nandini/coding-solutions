edb313c43c6bae23fd4c05b28b6963519a6eea97
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-15 12:55:56 +0530

    ✅ Max Consecutive Ones | Easy | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-09-15

--- leetcode/easy/0485-max-consecutive-ones/solution.java
+++ leetcode/easy/0485-max-consecutive-ones/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int c = 0, max = 0;
+        int c = 1, max = 1;
         if(nums[0] == 1) c++;
         for(int i = 1; i < nums.length; i++){
             if(nums[i-1] == 1 && nums[i] == 1){
                 c++;
                 max = Math.max(c, max);
             }
-            else c = 0;
+            else c = 1;
         }
         return max;
     }
