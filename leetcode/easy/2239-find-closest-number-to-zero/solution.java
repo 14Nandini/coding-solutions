80dcc2ce58d1e98d3636884b3336da9494b69131
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-16 20:56:49 +0530

    ✅ Find Closest Number to Zero | Easy | Java | Runtime: 2 ms (82.88%) | Memory: 46.8 MB | 2026-09-16

---
+++ leetcode/easy/2239-find-closest-number-to-zero/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int minDist = Math.abs(nums[0]);
+        int closest = nums[0];
+        for(int num : nums){
+            int abs = Math.abs(num);
+            if(abs < minDist || (abs == minDist && num > closest)){
+                closest = num;
+                minDist = abs;
+            }
+        }
+        return closest;
+    }
+}
