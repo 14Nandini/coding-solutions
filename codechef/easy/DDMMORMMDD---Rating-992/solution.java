a9e856cccb6f3439e95b4ea73f45e26ee6e80bea
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-23 20:24:10 +0530

    ✅ DDMMORMMDD - Rating 992 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/easy/DDMMORMMDD---Rating-992/solution.java
+++ codechef/easy/DDMMORMMDD---Rating-992/solution.java
@@ -7,28 +7,28 @@ public class Main {
 
         while (t-- > 0) {
             String s = scanner.next();
-            
-            int first = Integer.parseInt(s.substring(0,2));
-            int second = Integer.parseInt(s.substring(3,5));
-            
-            if(first < 12 && second < 12) System.out.println("BOTH");
-            else if(first < 12) System.out.println("MM/DD/YYYY");
-            else System.out.println("DD/MM/YYYY");
-            
-            // String[] str = s.split("/");
-            // String r = "";
-            // int[] res = new int[str.length];
-            // for(int i = 0; i < str.length; i++){
-            //     res[i] = Integer.parseInt(str[i]);
-            //     if(1 <= res[i] && res[i] <= 31) {
-            //         if(res[i] <= 12) r = r + "MM/";
-            //         else r = r + "DD/";
-            //     }
-            //     else r = r + "YYYY";
-            // }
-            // String both = "MM/MM/YYYY";
-            // if(r.equals(both)) System.out.println("BOTH");
-            // else System.out.println(r);
+
+            String[] str = s.split("/");
+            String r = "";
+            int[] res = new int[str.length];
+            for(int i = 0; i < str.length; i++){
+                res[i] = Integer.parseInt(str[i]);
+                if(1 <= res[i] && res[i] <= 31) {
+                    if(res[i] <= 12) r = r + "MM/";
+                    else r = r + "DD/";
+                }
+                else r = r + "YYYY";
+            }
+            String both = "MM/MM/YYYY";
+            if(r.equals(both)) System.out.println("BOTH");
+            else System.out.println(r);
         }
     }
 }
+
+// int first = Integer.parseInt(s.substring(0,2));
+// int second = Integer.parseInt(s.substring(3,5));
+            
+// if(first < 12 && second < 12) System.out.println("BOTH");
+// else if(first < 12) System.out.println("MM/DD/YYYY");
+// else System.out.println("DD/MM/YYYY");
