fcc4a9c8028fea780c09ae092fbfee80eafc15e2
Nandini Gedela <23A81A0581@sves.org.in>
2026-09-26 14:28:53 +0530

    ✅ Deque Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ gfg/basic/dequeue-traversal/solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for(int val : deq) System.out.print(val +  " ");
+        System.out.println();
+    }
+}
